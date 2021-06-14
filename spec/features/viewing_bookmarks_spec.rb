a2bdf0703dbf80c9f5f8b37175e57d5f3d073ba2
Fabio <alessf13@gmail.com>
2021-06-14 16:19:21 +0100

    Bookmark class created, unit and feature tests passing

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,6 +1,17 @@
+feature 'Navigating the page' do
+	 scenario 'visiting the index page' do
+    visit('/')
+
+		 expect(page).to have_content "Bookmark Manager"
+	 end
+end
+
 feature 'Viewing bookmarks' do
-	scenario 'visiting the index page' do
-		visit('/')
-		expect(page).to have_content "Bookmark Manager"
-	end
+	 scenario 'A user can see bookmarks' do
+		visit('/bookmarks')
+
+		  expect(page).to have_content "http://www.makersacademy.com"
+		  expect(page).to have_content "http://www.destroyallsoftware.com"
+		  expect(page).to have_content "http://www.google.com"
+	  end
 end
